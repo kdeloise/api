46f6d0c54f1d7c3f83a439da46f052ec257a0b5e
Kadabra Deloise <kdeloise@am-j4.msk.21-school.ru>
2020-08-13 02:33:32 -0700

    delete

--- config.py
+++ config.py
@@ -1,9 +1,6 @@
-TOKEN1 = "813910723:AAGmCxXB--cim_45RaAOabN80X1w7QvH70c"
-TOKEN2 = "1004918507:AAEjgYogrzOdEWPG1DoO1rZazVnv4HCV3I0"
-
 DATABASE = {
         "database": "base_for_api",
-        "user": "postgres",
+        "user": "kdeloise",
         "password": "181192",
         "host": "127.0.0.1",
         "port": "5432"
